4a9b4f7b0a191cf0258552abebff7255ab2719e8
mmoralezc <mmoralez@provinciamicrocreditos.com>
2023-12-01 13:51:31 -0300

    Clase Services

---
+++ Services/OpportunityBaseServices.cs
@@ -0,0 +1,18 @@
+﻿using FogabaMailService.Models;
+
+namespace Servicios_CRM_Replica.Services
+{
+    public class OpportunityBaseServices
+    {
+        private ProvMicroOpContext _dbProvMicroOpcontext;
+        private OpportunityBase _oppBase;
+        private Contacto _contact;
+        private SystemUserBase _user;
+
+        //Logica de negocio
+        //Reglas de negocio
+        //Se inyecta en el controlador
+        //Que datos de la oportunidad/contacto/usuario deberia exponer?
+
+    }
+}
